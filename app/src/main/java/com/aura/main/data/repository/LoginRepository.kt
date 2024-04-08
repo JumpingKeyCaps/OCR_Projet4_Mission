5382b5c665cc0927f1442e0e783785ac7289b871
Samuel Lebailly <sam.meusa@gmail.com>
2024-04-09 00:05:33 +0200

    fix(data/service): Update to use the new AuraApiService.

--- app/src/main/java/com/aura/main/data/repository/LoginRepository.kt
+++ app/src/main/java/com/aura/main/data/repository/LoginRepository.kt
@@ -1,14 +1,14 @@
 package com.aura.main.data.repository
 
-import com.aura.main.data.service.LoginApiService
+import com.aura.main.data.service.AuraApiService
 import com.aura.main.model.LoginRequest
 import com.aura.main.model.LoginResponse
 import javax.inject.Inject
 
-class LoginRepository @Inject constructor(private val loginApiService: LoginApiService) {
+class LoginRepository @Inject constructor(private val auraApiService: AuraApiService) {
 
     suspend fun login(user: LoginRequest): LoginResponse {
-        return loginApiService.login(user)
+        return auraApiService.login(user)
     }
 
 }
