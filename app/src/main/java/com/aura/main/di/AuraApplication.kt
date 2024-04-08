d3289124b3513b335171af613f8ed76bb0aae9ba
Samuel Lebailly <sam.meusa@gmail.com>
2024-04-08 21:46:31 +0200

    feat(di): Add Aura application class to use Hilt correctly.

---
+++ app/src/main/java/com/aura/main/di/AuraApplication.kt
@@ -0,0 +1,11 @@
+package com.aura.main.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Application Class to use Hilt
+ */
+@HiltAndroidApp
+class AuraApplication: Application() {
+}
