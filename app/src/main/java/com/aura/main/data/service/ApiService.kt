460cf43504cafa1aa55f3907a3fad35cbe8a2c5f
Samuel Lebailly <sam.meusa@gmail.com>
2024-04-08 23:10:02 +0200

    feat(data/service): add the retrofit @Body annotation to the parameter of login function.

--- app/src/main/java/com/aura/main/data/service/ApiService.kt
+++ app/src/main/java/com/aura/main/data/service/ApiService.kt
@@ -2,9 +2,10 @@ package com.aura.main.data.service
 
 import com.aura.main.model.LoginRequest
 import com.aura.main.model.LoginResponse
+import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface ApiService {
     @POST("/login")
-    suspend fun login(user: LoginRequest): LoginResponse
+    suspend fun login(@Body loginRequest: LoginRequest): LoginResponse
 }
