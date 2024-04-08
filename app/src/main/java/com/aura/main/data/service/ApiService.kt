9071483bc9bf5db750062a318552a074fbf92b9e
Samuel Lebailly <sam.meusa@gmail.com>
2024-04-09 01:11:27 +0200

    feat(data/service): Update the ApiService interface to add transfer request method.

--- app/src/main/java/com/aura/main/data/service/ApiService.kt
+++ app/src/main/java/com/aura/main/data/service/ApiService.kt
@@ -1,11 +1,40 @@
 package com.aura.main.data.service
 
-import com.aura.main.model.LoginRequest
-import com.aura.main.model.LoginResponse
+import com.aura.main.model.login.LoginRequest
+import com.aura.main.model.login.LoginResponse
+import com.aura.main.model.transfer.TransferRequest
+import com.aura.main.model.transfer.TransferResponse
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface ApiService {
+
+    /**
+     * Method for a login request to the server (POST)
+     *
+     * (Use of "suspend" to allow asynchronous call inside coroutine)
+     *
+     * @param loginRequest a login request object.
+     * @return a LoginResponse with a boolean to know if the login is successful or not.
+     */
     @POST("/login")
     suspend fun login(@Body loginRequest: LoginRequest): LoginResponse
+
+
+    /**
+     * Method for a transfer request to the server (POST)
+     *
+     * (Use of "suspend" to allow asynchronous call inside coroutine)
+     *
+     * @param transferRequest a transfer request object.
+     * @return a TransferResponse with a boolean to know if the transfer is successful or not.
+     */
+    @POST("/transfer")
+    suspend fun transfer(@Body transferRequest: TransferRequest): TransferResponse
+
+
+
+
+
+
 }
