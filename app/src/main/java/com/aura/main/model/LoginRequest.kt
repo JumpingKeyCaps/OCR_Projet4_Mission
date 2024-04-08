b1c68882cc9efafbd066826b52985add6049f6ef
Samuel Lebailly <sam.meusa@gmail.com>
2024-04-08 21:50:12 +0200

    feat(model): Add LoginRequest data model to create a login request to the server.

---
+++ app/src/main/java/com/aura/main/model/LoginRequest.kt
@@ -0,0 +1,9 @@
+package com.aura.main.model
+
+/**
+ * Data class who represent a LoginRequest data.
+ *
+ * @param id  The user identifier to login.
+ * @param password  The user password to login.
+ */
+data class LoginRequest(val id: String, val password: String)
