75990f82fe0c151289f8402e3e5dd881894f6127
Samuel Lebailly <sam.meusa@gmail.com>
2024-04-08 22:49:47 +0200

    feat(di): Create a AppConfig file with all network Url static content.

---
+++ app/src/main/java/com/aura/main/di/AppConfig.kt
@@ -0,0 +1,9 @@
+package com.aura.main.di
+
+/**
+ * Object App Config to easy use of all URL in app
+ */
+object AppConfig {
+    //the server base URL
+    const val BASE_URL = "https://votre-url.com"
+}
