cf826f77e0c9c843ae23af58a510be73a1eebf74
Samuel Lebailly <sam.meusa@gmail.com>
2024-04-08 19:42:40 +0200

    build(gradle): Add dependencies to use Hilt.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,11 @@
+import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
+
 plugins {
   id("com.android.application")
   id("org.jetbrains.kotlin.android")
+  id("com.google.dagger.hilt.android")
+  id("kotlin-kapt")
+  kotlin("kapt")
 }
 
 android {
@@ -43,12 +48,22 @@ dependencies {
   implementation("androidx.annotation:annotation:1.6.0")
   implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
+  //lifecycle
   implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
   implementation ("androidx.activity:activity-ktx:1.8.2")
   implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
 
+  //coroutine
   implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
 
+  //Hilt
+  implementation ("com.google.dagger:hilt-android:2.44")
+  kapt ("com.google.dagger:hilt-compiler:2.44")
+
+  //retrofit + OkHttp3 + Gson
+  implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+  implementation ("com.squareup.okhttp3:okhttp:4.9.3")
+  implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 
 
   testImplementation("junit:junit:4.13.2")
