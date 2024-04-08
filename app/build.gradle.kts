e6a541a6f4650dd091fec1d93707cf4d053e7875
Samuel Lebailly <sam.meusa@gmail.com>
2024-04-08 17:38:29 +0200

    build(gradle): add activity-ktx dependency to use "by viewModels()" in activity.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
   implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
   implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+  implementation ("androidx.activity:activity-ktx:1.8.2")
 
 
   testImplementation("junit:junit:4.13.2")
