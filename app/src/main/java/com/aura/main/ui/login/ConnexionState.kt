03711781816e065a14a99e0dc7cfa7f17861228e
Samuel Lebailly <sam.meusa@gmail.com>
2024-04-08 17:11:33 +0200

    feat(ui/login): Add a enumeration class for all the possible States of the login screen.

---
+++ app/src/main/java/com/aura/main/ui/login/ConnexionState.kt
@@ -0,0 +1,12 @@
+package com.aura.main.ui.login
+
+/**
+ * Enumeration of all different available state of the login screen.
+ */
+enum class ConnexionState {
+    INITIAL,
+    CHAMPS_REMPLIS,
+    ERREUR_CONNEXION,
+    CONNEXION_EN_COURS,
+    CONNEXION_REUSSIE
+}
