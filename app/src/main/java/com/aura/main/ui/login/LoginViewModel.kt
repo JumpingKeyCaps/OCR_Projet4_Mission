115216713826ead611c7cb247d44db6cebd40318
Samuel Lebailly <sam.meusa@gmail.com>
2024-04-08 22:53:41 +0200

    feat(ui/login): Update ViewModel to use the new available state.

--- app/src/main/java/com/aura/main/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/aura/main/ui/login/LoginViewModel.kt
@@ -1,16 +1,23 @@
 package com.aura.main.ui.login
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.aura.main.data.repository.LoginRepository
+import com.aura.main.model.LoginRequest
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import javax.inject.Inject
+
 
 /**
  * The ViewModel for the Login Activity.
  */
-class LoginViewModel : ViewModel()  {
+@HiltViewModel
+class LoginViewModel @Inject constructor(private val loginRepository: LoginRepository) : ViewModel()  {
 
     private val _etat = MutableStateFlow(ConnexionState.INITIAL)
     val etat: StateFlow<ConnexionState> = _etat
@@ -25,24 +32,32 @@ class LoginViewModel : ViewModel()  {
         }
     }
 
-    fun seConnecter() {
+    fun seConnecter(identifier: String, motDePasse: String) {
         // Use viewModelScope for coroutines related to the Activity lifecycle
         viewModelScope.launch {
             _etat.value = ConnexionState.CONNEXION_EN_COURS
-            // Simuler une connexion (could be replaced with actual network call)
 
-            //todochange by apicall
-            delay(15000)
 
-            if (true) {
-                // Success !
-                _etat.value = ConnexionState.CONNEXION_REUSSIE
+            try {
+                val loginResponse = loginRepository.login(LoginRequest(identifier, motDePasse))
 
-            } else {
-                //erreur de connexion
+                if(loginResponse.granted){
+                    // Connexion accepted ! successful login response
+                    _etat.value = ConnexionState.CONNEXION_REUSSIE
+                    Log.d("connectMMM", "Connexion Ok")
+                }else{
+                    //Connexion refused.
+                    _etat.value = ConnexionState.CONNEXION_ECHEC
+                    Log.d("connectMMM", "seConnecter: echec de connexion !")
+                }
+
+            } catch (e: Exception) {
+                // Handle network errors, server errors, etc.
                 _etat.value = ConnexionState.ERREUR_CONNEXION
-
+                Log.d("connectMMM", "seConnecter: erreur rsx !")
             }
+
+
         }
 
     }
