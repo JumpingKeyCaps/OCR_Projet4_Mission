819d48261cfee05e12ace78e85680da82ac5805b
Samuel Lebailly <sam.meusa@gmail.com>
2024-04-09 04:36:20 +0200

    feat(data/repository): Update the getUserAccount method to use the Response<> to create a list of UserAccount.

--- app/src/main/java/com/aura/main/data/repository/HomeRepository.kt
+++ app/src/main/java/com/aura/main/data/repository/HomeRepository.kt
@@ -2,6 +2,8 @@ package com.aura.main.data.repository
 
 import com.aura.main.data.service.AuraApiService
 import com.aura.main.model.home.UserAccount
+import com.google.gson.Gson
+import retrofit2.Response
 import javax.inject.Inject
 
 /**
@@ -17,7 +19,20 @@ class HomeRepository @Inject constructor(private val auraApiService: AuraApiServ
      * @return a list of the user accounts.
      */
     suspend fun getUserAccounts(id: String): List<UserAccount> {
-        return auraApiService.getUserAccounts(id)
+        //on recupere la reponse du service
+        val response = auraApiService.getUserAccounts(id)
+
+        if (!response.isSuccessful) {
+            //echec de la reponse
+            throw Exception("Error: ${response.code()}")
+        }else{
+            //reponse ok
+            //on deserialise la Json en une list de UserAccount
+            val gson = Gson()
+            //on return la reponse avec notre list.
+            return gson.fromJson(response.body()?.toString(), Array<UserAccount>::class.java).toList()
+        }
+
     }
 
 }
