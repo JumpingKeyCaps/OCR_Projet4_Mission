021a9187bb83b0e2ec1a1c6a689ceda966e9fb03
Samuel Lebailly <sam.meusa@gmail.com>
2024-04-09 02:44:57 +0200

    docs(data/repository): Add Kotlin Docs.

--- app/src/main/java/com/aura/main/data/repository/LoginRepository.kt
+++ app/src/main/java/com/aura/main/data/repository/LoginRepository.kt
@@ -5,8 +5,17 @@ import com.aura.main.model.login.LoginRequest
 import com.aura.main.model.login.LoginResponse
 import javax.inject.Inject
 
+/**
+ * Repository for the Login data.
+ * Inject the used API via constructor.
+ */
 class LoginRepository @Inject constructor(private val auraApiService: AuraApiService) {
 
+    /**
+     * Method to login the user to his account.
+     *
+     * @param user a LoginRequest object.
+     */
     suspend fun login(user: LoginRequest): LoginResponse {
         return auraApiService.login(user)
     }
