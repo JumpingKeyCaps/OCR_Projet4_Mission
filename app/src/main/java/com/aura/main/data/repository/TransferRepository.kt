82aac50c81d6c8ea68c166665de7f23be03a9c3a
Samuel Lebailly <sam.meusa@gmail.com>
2024-04-09 02:43:46 +0200

    feat(data/repository): Add a Transfer repository.

---
+++ app/src/main/java/com/aura/main/data/repository/TransferRepository.kt
@@ -0,0 +1,24 @@
+package com.aura.main.data.repository
+
+import com.aura.main.data.service.AuraApiService
+import com.aura.main.model.transfer.TransferRequest
+import com.aura.main.model.transfer.TransferResponse
+import javax.inject.Inject
+
+/**
+ * Repository for the Transfer data.
+ * Inject the used API via constructor.
+ */
+class TransferRepository @Inject constructor(private val auraApiService: AuraApiService) {
+
+    /**
+     * Method to done a transfer.
+     *
+     *  @param transferRequest a transfer request object.
+     *  @return  a TransferResponse object with the transfer result.
+     */
+    suspend fun transfer(transferRequest: TransferRequest): TransferResponse {
+        return auraApiService.transfer(transferRequest)
+    }
+
+}
