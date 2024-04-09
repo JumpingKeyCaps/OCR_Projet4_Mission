f529b85dff722b82eada05422d07aba35d063545
Samuel Lebailly <sam.meusa@gmail.com>
2024-04-09 20:46:59 +0200

    feat(data/repository): fix getUserAccount() method to provide good return type.

--- app/src/main/java/com/aura/main/data/repository/HomeRepository.kt
+++ app/src/main/java/com/aura/main/data/repository/HomeRepository.kt
@@ -1,9 +1,8 @@
 package com.aura.main.data.repository
 
+import android.util.Log
 import com.aura.main.data.service.AuraApiService
 import com.aura.main.model.home.UserAccount
-import com.google.gson.Gson
-import retrofit2.Response
 import javax.inject.Inject
 
 /**
@@ -21,16 +20,12 @@ class HomeRepository @Inject constructor(private val auraApiService: AuraApiServ
     suspend fun getUserAccounts(id: String): List<UserAccount> {
         //on recupere la reponse du service
         val response = auraApiService.getUserAccounts(id)
-
         if (!response.isSuccessful) {
             //echec de la reponse
             throw Exception("Error: ${response.code()}")
         }else{
             //reponse ok
-            //on deserialise la Json en une list de UserAccount
-            val gson = Gson()
-            //on return la reponse avec notre list.
-            return gson.fromJson(response.body()?.toString(), Array<UserAccount>::class.java).toList()
+            return response.body()?: emptyList()
         }
 
     }
