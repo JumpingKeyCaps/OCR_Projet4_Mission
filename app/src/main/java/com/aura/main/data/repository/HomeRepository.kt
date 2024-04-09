c5a31923a2a9de254d9e0f5a0fd6c2cde2cceb84
Samuel Lebailly <sam.meusa@gmail.com>
2024-04-09 02:43:14 +0200

    feat(data/repository): Add a Home repository.

---
+++ app/src/main/java/com/aura/main/data/repository/HomeRepository.kt
@@ -0,0 +1,23 @@
+package com.aura.main.data.repository
+
+import com.aura.main.data.service.AuraApiService
+import com.aura.main.model.home.UserAccount
+import javax.inject.Inject
+
+/**
+ * Repository for the Home data.
+ * Inject the used API via constructor.
+ */
+class HomeRepository @Inject constructor(private val auraApiService: AuraApiService) {
+
+    /**
+     * Method to get all the user accounts.
+     *
+     * @param id the user identifier.
+     * @return a list of the user accounts.
+     */
+    suspend fun getUserAccounts(id: String): List<UserAccount> {
+        return auraApiService.getUserAccounts(id)
+    }
+
+}
