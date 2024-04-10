00ed4695b6adad65d1ade0c370a9d91a8bc01852
Samuel Lebailly <sam.meusa@gmail.com>
2024-04-10 17:15:11 +0200

    feat(ui/transfer): Create new transfer states for the transfer screen.

---
+++ app/src/main/java/com/aura/main/ui/transfer/TransferState.kt
@@ -0,0 +1,13 @@
+package com.aura.main.ui.transfer
+
+/**
+ * Enumeration of all different available state of the transfer screen.
+ */
+enum class TransferState {
+    IDLE,
+    FIELDS_OK,
+    LOADING,
+    ERROR,
+    FAIL,
+    SUCCESS
+}
