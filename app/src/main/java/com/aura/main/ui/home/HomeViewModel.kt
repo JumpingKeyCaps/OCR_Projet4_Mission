7ee44abb5e0cddd8d53c4b5fff319f8acd220b14
Samuel Lebailly <sam.meusa@gmail.com>
2024-04-10 15:37:19 +0200

    feat(ui/home): Add getHomeInfoMessageToShow() method in the home viewmodel to return the string to display to the user in different home states.

--- app/src/main/java/com/aura/main/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/aura/main/ui/home/HomeViewModel.kt
@@ -1,10 +1,13 @@
 package com.aura.main.ui.home
 
 import android.util.Log
+import androidx.annotation.StringRes
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.aura.R
 import com.aura.main.data.repository.HomeRepository
 import com.aura.main.model.home.UserAccount
+import com.aura.main.ui.login.ConnexionState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -54,4 +57,23 @@ class HomeViewModel @Inject constructor(private val homeRepository: HomeReposito
             }
         }
     }
+
+
+    /**
+     * Method to get the informative message to display to the user.
+     * @param homeState the current home state of the activity.
+     */
+    @StringRes
+    fun getHomeInfoMessageToShow(homeState: HomeState): Int {
+        return when(homeState){
+            HomeState.LOADING -> R.string.home_conn_loading
+            HomeState.ERROR -> R.string.home_conn_error
+            else -> 0
+        }
+
+    }
+
+
+
+
 }
