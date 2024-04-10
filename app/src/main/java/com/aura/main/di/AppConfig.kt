84e48a4b76b33fb33ec637c9d506c01bd431432a
Samuel Lebailly <sam.meusa@gmail.com>
2024-04-10 17:14:05 +0200

    feat(di): Change API source URL to debug with a real device.

--- app/src/main/java/com/aura/main/di/AppConfig.kt
+++ app/src/main/java/com/aura/main/di/AppConfig.kt
@@ -5,7 +5,7 @@ package com.aura.main.di
  */
 object AppConfig {
     //the server base URL
-    const val BASE_URL = "http://10.0.2.2:8080/"
-  //  const val BASE_URL = "http://192.168.1.23:8080/"
+    //const val BASE_URL = "http://10.0.2.2:8080/"
+    const val BASE_URL = "http://192.168.1.23:8080/"
 
 }
