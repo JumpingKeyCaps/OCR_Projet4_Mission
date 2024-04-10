f1d72c541ab841cb7ab5d336d2a0db93a5833537
Samuel Lebailly <sam.meusa@gmail.com>
2024-04-10 18:03:51 +0200

    clean(ui/home): Cleaning the Home ViewModel code and add kotlin docs.

--- app/src/main/java/com/aura/main/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/aura/main/ui/home/HomeViewModel.kt
@@ -1,15 +1,12 @@
 package com.aura.main.ui.home
 
-import android.util.Log
 import androidx.annotation.StringRes
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.aura.R
 import com.aura.main.data.repository.HomeRepository
 import com.aura.main.model.home.UserAccount
-import com.aura.main.ui.login.ConnexionState
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -17,7 +14,7 @@ import javax.inject.Inject
 
 /**
  * The ViewModel for the Home Activity.
- * - Inject the repository dependency in the constructor
+ * Inject the repository dependency in the constructor
  */
 @HiltViewModel
 class HomeViewModel @Inject constructor(private val homeRepository: HomeRepository) : ViewModel()  {
@@ -25,22 +22,30 @@ class HomeViewModel @Inject constructor(private val homeRepository: HomeReposito
     private val _etat = MutableStateFlow(HomeState.IDLE)
     val etat: StateFlow<HomeState> = _etat
 
-
+    /**
+     * The User Account StateFlow
+     */
     private val _userAccount = MutableStateFlow<UserAccount?>(null)
     val userAccount: StateFlow<UserAccount?> = _userAccount
 
+
+    /**
+     * Method to get the UserAccount from the user ID
+     *
+     * @param idUser the User ID.
+     */
     fun getUserAccounts(idUser: String) {
         viewModelScope.launch {
             _etat.value = HomeState.LOADING
 
             //todo DEBUG ONLY -------------
-        //    delay(5000)
-            //todo --Fakeerror------------------
-            //todo -------
+            //    delay(5000)
+            //todo --------------------
 
             try {
+                //call the repository methode to get the user Accounts list
                 val userAccounts = homeRepository.getUserAccounts(idUser)
-
+                // get the main account from the list returned
                 val mainAccount = userAccounts.firstOrNull { it.main }
                 _userAccount.value = mainAccount
                 if(mainAccount!=null){
@@ -48,12 +53,8 @@ class HomeViewModel @Inject constructor(private val homeRepository: HomeReposito
                 }else{
                     _etat.value = HomeState.ERROR
                 }
-                Log.d("Homedebug", "ViewModel Success ! : ${mainAccount.toString()}")
-
             } catch (e: Exception) {
                 _etat.value = HomeState.ERROR
-                Log.d("Homedebug", "ViewModel Error Exception ! : ${e.toString()}")
-
             }
         }
     }
@@ -62,6 +63,7 @@ class HomeViewModel @Inject constructor(private val homeRepository: HomeReposito
     /**
      * Method to get the informative message to display to the user.
      * @param homeState the current home state of the activity.
+     * @return the reference of the string resource to use.
      */
     @StringRes
     fun getHomeInfoMessageToShow(homeState: HomeState): Int {
@@ -70,10 +72,6 @@ class HomeViewModel @Inject constructor(private val homeRepository: HomeReposito
             HomeState.ERROR -> R.string.home_conn_error
             else -> 0
         }
-
     }
-
-
-
 
 }
