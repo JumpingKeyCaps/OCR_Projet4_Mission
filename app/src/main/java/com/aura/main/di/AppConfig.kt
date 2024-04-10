b740ab1eee23186eae6263920688f04b7dc45c3a
Samuel Lebailly <sam.meusa@gmail.com>
2024-04-10 17:39:33 +0200

    clean(di): Cleaning the Aura App config code.

--- app/src/main/java/com/aura/main/di/AppConfig.kt
+++ app/src/main/java/com/aura/main/di/AppConfig.kt
@@ -4,8 +4,9 @@ package com.aura.main.di
  * Object App Config to easy use of all URL in app
  */
 object AppConfig {
-    //the server base URL
+    /** the server base URL for emulator*/
     //const val BASE_URL = "http://10.0.2.2:8080/"
-    const val BASE_URL = "http://192.168.1.23:8080/"
 
+    /** The server base URL for real device on local network.*/
+    const val BASE_URL = "http://192.168.1.23:8080/"
 }
