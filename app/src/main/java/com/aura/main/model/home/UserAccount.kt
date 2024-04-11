4ff23d6f97eff73de1c56688ef8eb926cb45d87f
Samuel Lebailly <sam.meusa@gmail.com>
2024-04-11 18:32:10 +0200

    feat(model/home): Add Json key name to use with moshi and improve the field resistance to code obfuscation.

--- app/src/main/java/com/aura/main/model/home/UserAccount.kt
+++ app/src/main/java/com/aura/main/model/home/UserAccount.kt
@@ -1,5 +1,7 @@
 package com.aura.main.model.home
 
+import com.squareup.moshi.Json
+
 
 /**
  * Data class who represent a user account data.
@@ -8,4 +10,7 @@ package com.aura.main.model.home
  * @param main  to know if its the main account of the user.
  * @param balance The quantity of money on the account.
  */
-data class UserAccount(val id: String, val main: Boolean, val balance: Double)
+data class UserAccount(
+    @Json(name = "id") val id: String,
+    @Json(name = "main") val main: Boolean,
+    @Json(name = "balance") val balance: Double)
