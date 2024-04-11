baa7ac1ea3d4552d2ccec97f6c53b0867fcb9cdd
Samuel Lebailly <sam.meusa@gmail.com>
2024-04-11 02:30:52 +0200

    test(di): Change of the Api URL to work with the emulator.

--- app/src/main/java/com/aura/main/di/AppConfig.kt
+++ app/src/main/java/com/aura/main/di/AppConfig.kt
@@ -5,8 +5,8 @@ package com.aura.main.di
  */
 object AppConfig {
     /** the server base URL for emulator*/
-    //const val BASE_URL = "http://10.0.2.2:8080/"
+    const val BASE_URL = "http://10.0.2.2:8080/"
 
     /** The server base URL for real device on local network.*/
-    const val BASE_URL = "http://192.168.1.23:8080/"
+    //const val BASE_URL = "http://192.168.1.23:8080/"
 }
