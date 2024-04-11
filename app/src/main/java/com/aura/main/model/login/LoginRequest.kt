0ca7a6405e4236cc466aff186cf772c3be387c2c
Samuel Lebailly <sam.meusa@gmail.com>
2024-04-11 21:25:59 +0200

    feat(model/login): Minor visual fix for the code.

--- app/src/main/java/com/aura/main/model/login/LoginRequest.kt
+++ app/src/main/java/com/aura/main/model/login/LoginRequest.kt
@@ -8,6 +8,5 @@ import com.squareup.moshi.Json
  * @param id  The user identifier to login.
  * @param password  The user password to login.
  */
-data class LoginRequest(
-    @Json(name = "id") val id: String,
-    @Json(name = "password") val password: String)
+
+data class LoginRequest(@Json(name = "id") val id: String, @Json(name = "password") val password: String)
