b11f6804dba14654b2d19e4bda51e556eb968d04
Samuel Lebailly <sam.meusa@gmail.com>
2024-04-11 18:33:38 +0200

    feat(model/login): Add Json key name to use with moshi and improve the field resistance to code obfuscation.

--- app/src/main/java/com/aura/main/model/login/LoginRequest.kt
+++ app/src/main/java/com/aura/main/model/login/LoginRequest.kt
@@ -1,9 +1,13 @@
 package com.aura.main.model.login
 
+import com.squareup.moshi.Json
+
 /**
  * Data class who represent a LoginRequest data.
  *
  * @param id  The user identifier to login.
  * @param password  The user password to login.
  */
-data class LoginRequest(val id: String, val password: String)
+data class LoginRequest(
+    @Json(name = "id") val id: String,
+    @Json(name = "password") val password: String)
--- app/src/main/java/com/aura/main/model/login/LoginResponse.kt
+++ app/src/main/java/com/aura/main/model/login/LoginResponse.kt
@@ -1,8 +1,10 @@
 package com.aura.main.model.login
 
+import com.squareup.moshi.Json
+
 /**
  * Data class who represent a LoginResponse data from the server.
  *
  * @param granted  the reply from the server if the connexion is granted or not.
  */
-data class LoginResponse(val granted: Boolean)
+data class LoginResponse(@Json(name = "granted") val granted: Boolean)
