16c440ff9253e2896c941149d7789fa57738153d
Samuel Lebailly <sam.meusa@gmail.com>
2024-04-12 17:44:37 +0200

    feat(model/login): Update the login ViewModel to use the new login LCE and expose this state object via his stateflow with the repository received response.

--- app/src/main/java/com/aura/main/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/aura/main/ui/login/LoginViewModel.kt
@@ -1,17 +1,21 @@
 package com.aura.main.ui.login
 
+import android.os.Build
 import android.util.Log
-import androidx.annotation.StringRes
+import androidx.annotation.RequiresExtension
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.aura.R
 import com.aura.main.data.repository.LoginRepository
+import com.aura.main.model.login.LoginLCE
 import com.aura.main.model.login.LoginRequest
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import java.net.ConnectException
+import java.net.SocketTimeoutException
 import javax.inject.Inject
 
 
@@ -21,72 +25,65 @@ import javax.inject.Inject
 @HiltViewModel
 class LoginViewModel @Inject constructor(private val loginRepository: LoginRepository) : ViewModel()  {
 
-    /** The Login state Stateflow. */
-    private val _etat = MutableStateFlow(ConnexionState.INITIAL)
-    val etat: StateFlow<ConnexionState> = _etat
+    /** The Login LCE Stateflow. */
+    private val _lceState = MutableStateFlow<LoginLCE>(LoginLCE.LoginCurrent(fieldIsOK = false,granted = false))
+    val lceState: StateFlow<LoginLCE> = _lceState
 
-    /**
-     * Method to check if the user have enter all the required data to login.
-     *
-     * @param identifier the Id enter by the user.
-     * @param motDePasse the password enter by the user.
-     */
-    fun verifierChamps(identifier: String, motDePasse: String) {
-        if (identifier.isNotEmpty() && motDePasse.isNotEmpty()) {
-            //champ de saisie Ok !
-            _etat.value = ConnexionState.CHAMPS_REMPLIS
-        } else {
-            //champ de saisie manquant
-            _etat.value = ConnexionState.INITIAL
-        }
-    }
 
     /**
      * Method to login in the user account.
      *
      * @param identifier the Id of the user.
-     * @param motDePasse the password of the user.
+     * @param password the password of the user.
      */
-    fun seConnecter(identifier: String, motDePasse: String) {
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+    fun login(identifier: String, password: String){
         // Use viewModelScope for coroutines related to the Activity lifecycle
         viewModelScope.launch {
-            _etat.value = ConnexionState.CONNEXION_EN_COURS
+            _lceState.value = LoginLCE.LoginLoading(R.string.login_conn_running)
 
             try {
-                val loginResponse = loginRepository.login(LoginRequest(identifier, motDePasse))
+                val loginResponse = loginRepository.login(LoginRequest(identifier, password))
 
                 //todo REMOVE THIS FAKE DELAY --------------
-                   delay(3000)
+                delay(2000)
                 //todo -------------------------------------
 
                 if(loginResponse.granted){
                     // Connexion accepted ! successful login response
-                    _etat.value = ConnexionState.CONNEXION_REUSSIE
+                    _lceState.value = LoginLCE.LoginCurrent(fieldIsOK = true, granted = true)
                 }else{
                     //Connexion refused.
-                    _etat.value = ConnexionState.CONNEXION_ECHEC
+                    _lceState.value = LoginLCE.LoginError(R.string.login_conn_fail)
                 }
             } catch (e: Exception) {
                 // Handle network errors, server errors, etc.
-                _etat.value = ConnexionState.ERREUR_CONNEXION
-                Log.d("LOGINdebug", "seConnecter: ERROR : ${e.toString()} ")
+                Log.d("LOGINdebug", "login: ERROR : ${e.toString()} ")
+
+                when (e) {
+                    is SocketTimeoutException -> {_lceState.value = LoginLCE.LoginError(R.string.login_conn_error_server)} //server down
+                    is ConnectException -> {_lceState.value = LoginLCE.LoginError(R.string.login_conn_error_network)}  // network connexion down
+                    else -> { _lceState.value = LoginLCE.LoginError(R.string.login_conn_error_generik)} // other case
+                }
             }
         }
     }
 
-
     /**
-     * Method to get the informative message to display to the user.
-     * @param connexionState the current login state of the activity.
+     * Method to check if the user have enter all the required data to login.
+     *
+     * @param identifier the Id enter by the user.
+     * @param password the password enter by the user.
      */
-    @StringRes
-    fun getLoginInfoMessageToShow(connexionState: ConnexionState): Int {
-        return when(connexionState){
-            ConnexionState.ERREUR_CONNEXION -> R.string.login_conn_error
-            ConnexionState.CONNEXION_ECHEC -> R.string.login_conn_fail
-            ConnexionState.CONNEXION_EN_COURS -> R.string.login_conn_running
-            else -> 0
+    fun fieldsCheck(identifier: String, password: String){
+        if (identifier.isNotEmpty() && password.isNotEmpty()) {
+            //champ de saisie Ok !
+            _lceState.value = LoginLCE.LoginCurrent(fieldIsOK = true, granted = false)
+        } else {
+            //champ de saisie manquant
+            _lceState.value = LoginLCE.LoginCurrent(fieldIsOK = false, granted = false)
         }
     }
+
 
 }
