1dd1d674f2aac3baf895d387884ac750dc6c0a55
Samuel Lebailly <sam.meusa@gmail.com>
2024-04-12 17:41:09 +0200

    feat(model/login): Create a sealed class (pattern LCE) for the state objects of login Activity.

---
+++ app/src/main/java/com/aura/main/model/login/LoginLCE.kt
@@ -0,0 +1,16 @@
+package com.aura.main.model.login
+
+/**
+ * LCE for the login activity
+ *
+ * Classic Login Current Error pattern to Defines the different states of the login activity.
+ *
+ *  - LoginLoading : the state when the activity is loading some data.
+ *  - LoginSuccess : the state when the activity as successfully load data.
+ *  - LoginError : the state when the activity as an error to load data.
+ */
+sealed class LoginLCE {
+    data class LoginLoading(val loadingMessage: Int) : LoginLCE()
+    data class LoginCurrent(val fieldIsOK: Boolean, val granted: Boolean) : LoginLCE()
+    data class LoginError(val errorMessage: Int) : LoginLCE()
+}
