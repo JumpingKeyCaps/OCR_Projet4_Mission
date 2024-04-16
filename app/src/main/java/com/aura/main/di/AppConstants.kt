7dd1f1d622e71d4d45bcc2435ceb619a46f48557
Samuel Lebailly <sam.meusa@gmail.com>
2024-04-16 14:05:51 +0200

    feat(di): Add an object AppConstants to manage all the shared constants inside the app  +  Add a new Constant to the userId Key used by intent and sharedStateHandle.

---
+++ app/src/main/java/com/aura/main/di/AppConstants.kt
@@ -0,0 +1,12 @@
+package com.aura.main.di
+
+
+/**
+ * An object with all the constants used across the project.
+ */
+object AppConstants {
+
+    /** The Intent Extra / savedStateHandle  Key constant used for the user ID . */
+    const val KEY_USER_ID = "userId"
+
+}
