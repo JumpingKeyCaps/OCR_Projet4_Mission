24888cb3e91b1cdb4d80501260435a8b14ff100f
Samuel Lebailly <sam.meusa@gmail.com>
2024-04-16 22:08:16 +0200

    feat(di): Replace the AppConstants object by a sealed class with an companion object.

--- app/src/main/java/com/aura/main/di/AppConstants.kt
+++ app/src/main/java/com/aura/main/di/AppConstants.kt
@@ -4,9 +4,13 @@ package com.aura.main.di
 /**
  * An object with all the constants used across the project.
  */
-object AppConstants {
+sealed class AppConstants {
+    object Keys {
+        /** The Intent Extra / savedStateHandle  Key constant used for the user ID . */
+        const val KEY_USER_ID = "userId"
+        // Définissez d'autres clés constantes ici
+    }
 
-    /** The Intent Extra / savedStateHandle  Key constant used for the user ID . */
-    const val KEY_USER_ID = "userId"
+
 
 }
