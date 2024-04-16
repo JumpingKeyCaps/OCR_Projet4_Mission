a261e8a35277d211284bf8137ed98c14c7aad433
Samuel Lebailly <sam.meusa@gmail.com>
2024-04-16 16:11:32 +0200

    feat(model/transfer): Create a new data class for the TransferContent screen state.

---
+++ app/src/main/java/com/aura/main/model/transfer/TransferContent.kt
@@ -0,0 +1,9 @@
+package com.aura.main.model.transfer
+
+/**
+ * Transfer content screen state
+ *
+ * @param fieldIsOK all the screen text fields are completed.
+ * @param result the transfer result.
+ */
+data class TransferContent(val fieldIsOK: Boolean, val result: Boolean)
