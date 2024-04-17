99bf11d89bf941541f99b6be163474eff117ea8d
Samuel Lebailly <sam.meusa@gmail.com>
2024-04-17 15:22:46 +0200

    fix(ui/home): Remove the update userId method.

--- app/src/main/java/com/aura/main/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/aura/main/ui/home/HomeViewModel.kt
@@ -39,20 +39,10 @@ class HomeViewModel @Inject constructor(private val homeRepository: HomeReposito
      * Initialisation du ViewModel
      */
     init {
-        val savedUserId = savedStateHandle.get<String>(AppConstants.Keys.KEY_USER_ID) ?: ""
+        val savedUserId = savedStateHandle.get<String>(AppConstants.KEY_USER_ID) ?: ""
         userId = savedUserId
     }
 
-
-    /**
-     * Method to update the userId ans save it in the savedStateHandle.
-     *
-     * @param userId the new userid to update.
-     */
-    fun updateUserId(userId: String) {
-        this.userId = userId
-        savedStateHandle[AppConstants.Keys.KEY_USER_ID] = userId
-    }
 
 
     /**
