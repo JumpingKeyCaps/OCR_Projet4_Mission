7b37dfef8bc967bd0effc03be68fb0ef3690cc6d
Samuel Lebailly <sam.meusa@gmail.com>
2024-04-17 15:24:46 +0200

    fix(di): use now a companion object to the Keys in AppConstants sealed class.

--- app/src/main/java/com/aura/main/di/AppConstants.kt
+++ app/src/main/java/com/aura/main/di/AppConstants.kt
@@ -5,7 +5,7 @@ package com.aura.main.di
  * An object with all the constants used across the project.
  */
 sealed class AppConstants {
-    object Keys {
+    companion object Keys {
         /** The Intent Extra / savedStateHandle  Key constant used for the user ID . */
         const val KEY_USER_ID = "userId"
         // Définissez d'autres clés constantes ici
